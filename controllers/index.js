aa4d9d2345291d2f30c9753c1e5ae514bf98f21b
Alexander Leino <Alexander.Leino@yahoo.com>
2021-10-26 12:47:55 -0400

    before slugify is used

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,18 @@
 const router = require('express').Router()
+const blogPosts = require('../models/blogPosts')
 
 
+router.get('/', (req, res) => res.render('index', {
+    name: 'Alex',
+    date: new Date()               
 
-router.get('/', (req, res) => res.render('index'))
+}))
 router.get('/about', (req, res) => res.render('about'))
 router.get('/contact', (req, res) => res.render('contact'))
 router.get('/services', (req, res) => res.render('services'))
-router.get('/blog', (req, res) => res.render('blog'))
+router.get('/blog', (req, res) => {res.render('blog',{
+    post : blogPosts
+    })
+})
 
 module.exports = router
