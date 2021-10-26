fbe18e125d4a5cf65b74fb1c506e2a7b513dc255
Alexander Leino <Alexander.Leino@yahoo.com>
2021-10-26 10:46:21 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const exphbs  = require('express-handlebars')
+const controllers = require('./controllers')
+
+const PORT = 3001
+const app = express()
+
+app.engine('handlebars', exphbs())
+app.set('view engine', 'handlebars')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+app.use(controllers)
+
+app.listen(PORT, () => console.log(`Go to http://localhost:${PORT}`))
